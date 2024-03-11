c62b6de23ee2fe5a5b9d865abf114edf18af1e49
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2024-03-11 19:34:33 +0500

    CREATE playRound FUNCTION

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 function getComputerChoice() {
-    let randomNumber = Math.floor((Math.random()*10) % 3) + 1;
+  let randomNumber = Math.floor((Math.random() * 10) % 3) + 1;
 
-    return (randomNumber === 1 && "Rock") || (randomNumber === 2 && "Paper") || (randomNumber === 3 && "Scissors");
+  return (
+    (randomNumber === 1 && "Rock") ||
+    (randomNumber === 2 && "Paper") ||
+    (randomNumber === 3 && "Scissors")
+  );
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  return playerSelection == "rock" && computerSelection == "paper"
+    ? "You lose! Paper beats Rock!"
+    : playerSelection == "paper" && computerSelection == "rock"
+    ? "You win! Paper beats Rock!"
+    : playerSelection == "paper" && computerSelection == "scissors"
+    ? "You lose! Scissors beats Paper!"
+    : playerSelection == "scissors" && computerSelection == "paper"
+    ? "You win! Scissors beats Paper!"
+    : playerSelection == "scissors" && computerSelection == "rock"
+    ? "You lose! Rock beats Scissors!"
+    : playerSelection == "rock" && computerSelection == "scissors"
+    ? "You win! Rock beats Scissors!"
+    : "Draw!";
+}
+
+const playerSelection = prompt("Your selection: ", "Rock");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
